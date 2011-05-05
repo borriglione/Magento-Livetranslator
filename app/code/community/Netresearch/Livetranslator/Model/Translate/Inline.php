eee7e2c4a11f6cd77acfc946df412b9e48c0a25e
Mario Behrendt <mario.behrendt@netresearch.de>
2011-05-05 13:51:53 +0200

    NRFE-147: Activate inline translation if session value is true

--- app/code/community/Netresearch/Livetranslator/Model/Translate/Inline.php
+++ app/code/community/Netresearch/Livetranslator/Model/Translate/Inline.php
@@ -1,16 +1,36 @@
 <?php
 /**
- * @category    Mage
- * @package     Netresearch_Livetranslator
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Mage
+ * @package    Mage_Core
+ * @copyright  Copyright (c) 2011 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 
 /**
- * Inline Translations PHP part
+ * Enabling inline translation if set in session
  *
- * @category   Mage
- * @package     Netresearch_Livetranslator
- * @author     Thomas Kappel <thomas.kappel@netresearch.de>
+ * @category   Netresearch
+ * @package    Netresearch_Livetranslator
+ * @author     Mario Behrendt <mario.behrendt@netresearch.de>
  */
 class Netresearch_Livetranslator_Model_Translate_Inline extends Mage_Core_Model_Translate_Inline
 {
@@ -22,27 +42,10 @@ class Netresearch_Livetranslator_Model_Translate_Inline extends Mage_Core_Model_
      */
     public function isAllowed($store = null)
     {
-        if (is_null($store)) {
-            $store = Mage::app()->getStore();
-        }
-        if (!$store instanceof Mage_Core_Model_Store) {
-            $store = Mage::app()->getStore($store);
+        if (Mage::getSingleton('core/session')->getInlineTranslationEnabled()) {
+            return true;
         }
 
-        if (is_null($this->_isAllowed)) {
-            if (Mage::getDesign()->getArea() == 'adminhtml') {
-                $active = Mage::getStoreConfigFlag('dev/translate_inline/active_admin', $store);
-            } else {
-                $active = Mage::getStoreConfigFlag('dev/translate_inline/active', $store)
-			  or user has permission to translate...
-            }
-
-            $this->_isAllowed = $active && Mage::helper('core')->isDevAllowed($store);
-        }
-
-        $translate = Mage::getSingleton('core/translate');
-        /* @var $translate Mage_Core_Model_Translate */
-
-        return $translate->getTranslateInline() && $this->_isAllowed;
+        return parent::isAllowed($store);
     }
 }
